5b4dec9850326bf7ea8bf94bf6ef560d6f325a1c
Diego Rampim <drampim@gmail.com>
2017-09-03 19:21:13 -0300

    Banco

--- casadocodigo/app/infra/connectionFactory.js
+++ casadocodigo/app/infra/connectionFactory.js
@@ -5,10 +5,10 @@ function createDBConnection() {
 
 	if(!process.env.NODE_ENV){
         return 	mysql.createConnection({
-            host : 'localhost',
-            user : 'root',
-            password : 'root',
-            database : 'casadocodigo'
+            host : '45.33.101.25',
+            user : 'alura',
+            password : 'alura',
+            database : 'alura'
         });
     }
 
